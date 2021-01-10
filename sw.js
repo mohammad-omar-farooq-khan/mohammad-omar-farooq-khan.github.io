3164f278e9c8c9f908fc3c55703f81c87173287b
mohammad-omar-farooq-khan <72381765+mohammad-omar-farooq-khan@users.noreply.github.com>
2021-01-10 15:05:58 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('fox-store').then(function(cache) {
+     return cache.addAll([
+       'index.js' , "index.html' , 'whatsapp-1623579_1280.png'
+        ]);
+   })
+ );
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
