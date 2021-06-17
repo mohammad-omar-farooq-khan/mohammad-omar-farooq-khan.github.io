916d300022dfaf470100fa21afa8f4815da5d187
mohammad-omar-farooq-khan <72381765+mohammad-omar-farooq-khan@users.noreply.github.com>
2021-06-17 09:29:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ self.addEventListener('install', function(e) {
  e.waitUntil(
    caches.open('whats-any').then(function(cache) {
      return cache.addAll([
-       'index.js' , "index.html' , 'whatsapp-1623579_1280.png'
+       'index.js' , 'index.html' , 'whatsapp-1623579_1280.png'
         ]);
    })
  );
